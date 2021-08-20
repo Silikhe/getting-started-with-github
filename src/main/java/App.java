5a1fbe5543dfd7bb2fdbc4c84330cd750ccab94f
silikhe <silaswan@gmail.com>
2021-08-20 14:03:58 +0300

    add default port for heroku

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
+
+}
