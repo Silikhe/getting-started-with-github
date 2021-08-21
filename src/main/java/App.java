6306bbce3e87c6b1b3a40104aee6c1777712c837
silikhe <silaswan@gmail.com>
2021-08-21 11:42:01 +0300

    adding resources

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,15 @@
+//import module.*;
+import spark.ModelAndView;
+import spark.QueryParamsMap;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class App {
 
     static int getHerokuAssignedPort() {
@@ -9,6 +21,16 @@ public class App {
     }
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
+        System.out.println("Hey Silas");
+
+        get("/",(request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 
 }
